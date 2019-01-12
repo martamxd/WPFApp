7b6e66edc20072c34510e8d48a851a030a3dd6eb
M M <martamxd@wp.pl>
2019-01-12 21:50:35 +0100

    zapisaniedanych

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace WpfApp1
 {
@@ -20,9 +21,69 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=TOSHIBA;Initial Catalog=DB;Integrated Security=True");
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    sqlCon.Open();
+                SqlCommand sqlCmd = new SqlCommand("ContactAddOrEdit", sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.StoredProcedure;
+                sqlCmd.Parameters.AddWithValue("@mode", "Add");
+                sqlCmd.Parameters.AddWithValue("@ContactId", 0);
+                sqlCmd.Parameters.AddWithValue("@Imie", txtImie.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Nazwisko",txtNazwisko.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@NumerTelefonu",txtNrTel.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@Adres", txtAdres.Text.Trim());
+                sqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Zapisano!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtImie_TextCahnged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtNazwisko_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtNrTel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtAdres_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ tbl_Contacts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Imie] NVARCHAR(50) NULL, 
+    [Nazwisko] NVARCHAR(50) NULL, 
+    [NumerTelefonu] NVARCHAR(50) NULL, 
+    [Adres] NVARCHAR(50) NULL
+)
